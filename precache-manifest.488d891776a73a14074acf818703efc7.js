eadeea65d0bc98de2f30db8c1d5920a501749670
klevis merko <klevismerko@kleviss-MBP.attlocal.net>
2020-04-07 20:50:02 -0400

    Updates

---
+++ precache-manifest.488d891776a73a14074acf818703efc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ebe8e876f7d57393cd7ba3c5c2d5ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bfda115168a3aab65b39",
+    "url": "/monsters-rolodex/static/css/main.4d1dc204.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfda115168a3aab65b39",
+    "url": "/monsters-rolodex/static/js/main.fe499410.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
